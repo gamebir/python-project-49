8fa4c2ec394a35a849c0eb090b24504d243810ab
Egor Kalinovstiy <gamebirbibi@yandex.ru>
2022-08-23 19:00:41 +0900

    Контрольный

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
+from ..cli import welcome_user
+
 def main():
     print("Welcome to the Brain Games!")
+    print(welcome_user())
 
-
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
