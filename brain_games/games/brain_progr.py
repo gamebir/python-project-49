5998030aca65af1fe42c8cc5e5849b80650d324a
Egor Kalinovstiy <gamebirbibi@yandex.ru>
2022-10-06 18:44:55 +0900

    Исправление замечаний

--- brain_games/games/brain_progr.py
+++ brain_games/games/brain_progr.py
@@ -4,21 +4,21 @@ from random import choice, randint
 BEGIN_RANGE = 0
 END_RANGE = 100
 STEP_PROGR = 3
-LEGHT_PROGR = 10
+LENGHT_PROGR = 10
 TASK = 'What number is missing in the progression?'
 
 
-def generate_progression(step_progr, leght_progr):
+def generate_progression(step_progr, lenght_progr):
     """generating a sequence"""
     result = ''
     n = randint(BEGIN_RANGE, END_RANGE)
-    for i in range(n, n + (step_progr * leght_progr), step_progr):
+    for i in range(n, n + (step_progr * lenght_progr), step_progr):
         result += f'{i} '
     return result
 
 
 def brain_progression():
-    progression = generate_progression(STEP_PROGR, LEGHT_PROGR)
+    progression = generate_progression(STEP_PROGR, LENGHT_PROGR)
     symbol = choice(progression.split())
     question = progression.replace(symbol, '..')
     correct_answer = symbol
